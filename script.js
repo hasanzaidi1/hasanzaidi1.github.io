b4163f2dd9dd603ea23034567ab47924d40a02bc
hasanzaidi1 <hasanammarza@usf.edu>
2024-11-17 22:32:48 -0500

    v1.16.6

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const RESUME_FILE_NAME = "Resume_Professional_Final_v2.4.0.pdf";
+const RESUME_FILE_NAME = "Resume_v3.3.0-A-FS.pdf";
 
 // header scrolling effect
 $(window).on('scroll', function(){
